7e73de906f7dba154f60d63eebc4abd7f54a9b14
Marvin Vogt <m@rvinvogt.com>
2023-09-19 14:43:07 +0000

    Add basic run invariant to Service

--- src/anycastd/_main.py
+++ src/anycastd/_main.py
@@ -1,3 +1,4 @@
+import asyncio
 from dataclasses import dataclass
 
 from anycastd.healthcheck.base import BaseHealthcheck
@@ -15,3 +16,15 @@ class Service:
     name: str
     prefixes: tuple[BasePrefix, ...]
     health_checks: tuple[BaseHealthcheck, ...]
+
+    async def run(self) -> None:
+        """Run the service.
+
+        This will announce the prefixes when all health checks are
+        passing, and denounce them otherwise.
+        """
+        while True:
+            if all(check.is_healthy for check in self.health_checks):
+                asyncio.gather(prefix.announce() for prefix in self.prefixes)
+            else:
+                asyncio.gather(prefix.denounce() for prefix in self.prefixes)
