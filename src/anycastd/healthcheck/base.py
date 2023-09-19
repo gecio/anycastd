f35c68cd63a9f86b4392eed0b3d8ad28f11fe5a2
Marvin Vogt <m@rvinvogt.com>
2023-09-19 13:42:12 +0000

    Fix order of decorators

--- src/anycastd/healthcheck/base.py
+++ src/anycastd/healthcheck/base.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class BaseHealthcheck(ABC):
     """A healthcheck that represents a status."""
 
-    @abstractmethod
     @property
+    @abstractmethod
     def is_healthy(self) -> bool:
         """Whether the healthcheck is healthy."""
