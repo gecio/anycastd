6f87018eb336c265a51da7f550f2a6f61996a451
Marvin Vogt <m@rvinvogt.com>
2023-09-20 10:19:18 +0000

    Add private attributes and `__init__` to `BaseHealthcheck`

--- src/anycastd/healthcheck/base.py
+++ src/anycastd/healthcheck/base.py
@@ -7,6 +7,14 @@ class BaseHealthcheck(ABC):
 
     interval: datetime.timedelta
 
+    _last_checked: datetime.datetime | None = None
+    _last_healthy: bool = False
+
+    def __init__(self, interval: datetime.timedelta):
+        if not isinstance(interval, datetime.timedelta):
+            raise TypeError("Interval must be a timedelta.")
+        self.interval = interval
+
     @property
     @abstractmethod
     def is_healthy(self) -> bool:
