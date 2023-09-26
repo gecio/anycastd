a0c1b623228670c6b1bf2a2742ce86325ee59c5d
Marvin Vogt <m@rvinvogt.com>
2023-09-26 09:55:34 +0000

    Add tests to get result from cabourotte

--- tests/healthcheck/cabourotte/test_result.py
+++ tests/healthcheck/cabourotte/test_result.py
@@ -2,8 +2,13 @@ import datetime
 import json
 from typing import TypedDict
 
+import httpx
 import pytest
-from anycastd.healthcheck._cabourotte.result import Result
+from anycastd.healthcheck._cabourotte.result import Result, get_result
+from pytest_mock import MockerFixture
+from respx import MockRouter
+
+CABOUROTTE_URL = "http://[::1]:9013"
 
 # The result of a healthcheck as returned by the cabourotte API.
 _ResultData = TypedDict(
@@ -49,3 +54,39 @@ def test_result_from_api_json(data: _ResultData):
     assert result.message == data["message"]
     assert result.duration == data["duration"]
     assert result.source == data["source"]
+
+
+class TestGetResult:
+    """Test getting a result from the cabourotte API."""
+
+    @pytest.mark.parametrize(
+        "name,url",
+        [("example-http-check", CABOUROTTE_URL), ("example-dns-check", CABOUROTTE_URL)],
+    )
+    def test_get_made_to_correct_url(
+        self, name: str, url: str, respx_mock: MockRouter, mocker: MockerFixture
+    ):
+        """A GET request is made to the correct URL."""
+        result_url = url + f"/result/{name}"
+        mock_endpoint = respx_mock.get(result_url)
+        mocker.patch("anycastd.healthcheck._cabourotte.result.Result.from_json")
+
+        get_result(name, url=url)
+
+        request = mock_endpoint.calls.last.request
+        assert request.method == "GET"
+        assert request.url == result_url
+
+    def test_correct_result_returned(self, respx_mock):
+        """The correct result corresponding to API data is returned."""
+        json_data = json.dumps(example_result())
+        name = "example-api"
+        result_url = CABOUROTTE_URL + f"/result/{name}"
+
+        mock_endpoint = respx_mock.get(result_url)
+        mock_response = httpx.Response(status_code=200, content=json_data.encode())
+        mock_endpoint.return_value = mock_response
+
+        result = get_result(name, url=CABOUROTTE_URL)
+
+        assert result == Result.from_json(json_data)
