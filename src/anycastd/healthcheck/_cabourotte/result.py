6ec1fbc1b621b4ea1c021d909ec2981df2085738
Marvin Vogt <m@rvinvogt.com>
2023-09-26 09:56:22 +0000

    Implement `get_result()` passing tests

--- src/anycastd/healthcheck/_cabourotte/result.py
+++ src/anycastd/healthcheck/_cabourotte/result.py
@@ -1,5 +1,6 @@
 import datetime
 
+import httpx
 from pydantic import BaseModel, Field
 
 
@@ -18,3 +19,20 @@ class Result(BaseModel):
     def from_json(cls, data: str | bytes | bytearray) -> "Result":
         """Create a result from JSON returned by the cabourotte API."""
         return cls.model_validate_json(data)
+
+
+def get_result(name: str, *, url: str) -> Result:
+    """Get the result of a specific healthcheck.
+
+    Arguments:
+        name: The name of the healthcheck.
+        url: The URL of the cabourotte API.
+
+    Returns:
+        The result of the healthcheck.
+    """
+    with httpx.Client() as client:
+        response = client.get(f"{url}/result/{name}")
+        response.raise_for_status()
+
+    return Result.from_json(response.content)
