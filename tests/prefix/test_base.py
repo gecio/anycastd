af2a871770dee4ea0888cfeb65aaffbd821297ec
Marvin Vogt <m@rvinvogt.com>
2023-10-04 09:24:26 +0000

    Add `# type: ignore` for wanted `TypeError`

--- tests/prefix/test_base.py
+++ tests/prefix/test_base.py
@@ -8,7 +8,7 @@ from tests.dummy import DummyPrefix
 def test__init___non_network_raises_type_error():
     """Passing a non network prefix raises a TypeError."""
     with pytest.raises(TypeError):
-        DummyPrefix("2001:db8::/32")
+        DummyPrefix("2001:db8::/32")  # type: ignore
 
 
 def test__init__ip_network(example_networks: IPv4Network | IPv6Network):
