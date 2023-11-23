c6115cc228430d56ff838bb42917b8a752274b9a
Marvin Vogt <m@rvinvogt.com>
2023-11-23 14:44:48 +0000

    Implement `from_configuration` classmethod for `FRRPrefix` passing tests

--- src/anycastd/configuration/_frr.py
+++ src/anycastd/configuration/_frr.py
@@ -1,5 +1,6 @@
 from ipaddress import IPv4Network, IPv6Network
 from pathlib import Path
+from typing import Self
 
 from pydantic_settings import BaseSettings
 
@@ -8,3 +9,25 @@ class FRRPrefix(BaseSettings):
     prefix: IPv4Network | IPv6Network
     vrf: int | None = None
     vtysh: Path = Path("/usr/bin/vtysh")
+
+    @classmethod
+    def from_configuration(cls, config: str | dict) -> Self:
+        """Create an instance from the configuration format.
+
+        The configuration can be provided in two formats, a simplified format
+        containing only the prefix as a string, or a full format containing the
+        prefix as well as other optional options.
+
+        Args:
+            config: The configuration for the prefix.
+
+        Returns:
+            A new FRRPrefix instance.
+        """
+        match config:
+            case str():
+                return cls.model_validate({"prefix": config})
+            case dict():
+                return cls.model_validate(config)
+            case _:
+                raise TypeError(f"Invalid configuration type {type(config)}")
