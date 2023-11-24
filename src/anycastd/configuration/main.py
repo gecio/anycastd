2fe5765b1d6a49824bff98b440c3ef4f3c2d4fa8
Marvin Vogt <m@rvinvogt.com>
2023-11-24 11:25:28 +0000

    Add basic config initialization implementation passing tests

--- src/anycastd/configuration/main.py
+++ src/anycastd/configuration/main.py
@@ -1,10 +1,19 @@
 import tomllib
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Self
 
 from anycastd.configuration._cabourotte import CabourotteHealthcheck
 from anycastd.configuration._frr import FRRPrefix
 from anycastd.configuration.exceptions import ConfigurationError
+
+prefix_classes: dict[str, type] = {
+    "frrouting": FRRPrefix,
+}
+
+check_classes: dict[str, type] = {
+    "cabourotte": CabourotteHealthcheck,
+}
 
 
 @dataclass
@@ -14,6 +23,43 @@ class ServiceConfiguration:
     name: str
     prefixes: tuple[FRRPrefix, ...]
     checks: tuple[CabourotteHealthcheck, ...]
+
+    @classmethod
+    def from_name_and_options(cls, name: str, options: dict) -> Self:
+        """Create an instance from the configuration format.
+
+        Args:
+            name: The name of the service.
+            options: The configuration options for the service.
+
+        Returns:
+            A new ServiceConfiguration instance.
+
+        Raises:
+            KeyError: The configuration is missing a required key.
+            ValueError: The configuration contains an invalid value.
+        """
+        prefixes = []
+        for prefix_type, prefix_configs in options["prefixes"].items():
+            try:
+                prefix_class = prefix_classes[prefix_type]
+            except KeyError as exc:
+                raise ValueError(f"Unknown prefix type: {prefix_type}") from exc
+
+            for config in prefix_configs:
+                prefixes.append(prefix_class.from_configuration(config))
+
+        checks = []
+        for check_type, check_configs in options["checks"].items():
+            try:
+                check_class = check_classes[check_type]
+            except KeyError as exc:
+                raise ValueError(f"Unknown check type: {check_type}") from exc
+
+            for config in check_configs:
+                checks.append(check_class.from_configuration(config))
+
+        return cls(name=name, prefixes=tuple(prefixes), checks=tuple(checks))
 
 
 @dataclass
@@ -33,6 +79,15 @@ class MainConfiguration:
             ConfigurationError: The configuration could not be read or parsed.
         """
         config = _read_toml_configuration(path)
+        try:
+            return cls(
+                services=tuple(
+                    ServiceConfiguration.from_name_and_options(name, options)
+                    for name, options in config["services"].items()
+                )
+            )
+        except (KeyError, ValueError) as exc:
+            raise ConfigurationError(path, exc) from exc
 
 
 def _read_toml_configuration(path: Path) -> dict:
