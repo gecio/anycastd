8405a6ee4693ac9975b984bd09607b7f61f1703c
Marvin Vogt <m@rvinvogt.com>
2023-11-27 15:18:18 +0000

    Add new parent class for sub-configurations, creating a generic `from_configuration` method

--- src/anycastd/_configuration/healthcheck/_cabourotte.py
+++ src/anycastd/_configuration/healthcheck/_cabourotte.py
@@ -1,5 +1,4 @@
 import datetime
-from typing import Self
 
 from anycastd._configuration.sub import HealthcheckConfiguration
 
@@ -18,29 +17,3 @@ class CabourotteHealthcheck(HealthcheckConfiguration):
     name: str
     url: str = DEFAULT_URL
     interval: datetime.timedelta = datetime.timedelta(seconds=5)
-
-    @classmethod
-    def from_configuration(cls, config: str | dict) -> Self:
-        """Create an instance from the configuration format.
-
-        The configuration can be provided in two formats, a simplified format
-        containing only the name of the healthcheck as a string, or a full format
-        containing the name as well as other optional options.
-
-        Args:
-            config: The configuration for the healthcheck.
-
-        Returns:
-            A new CabourotteHealthcheck instance.
-
-        Raises:
-            ValidationError: Failed to validate the configuration.
-            TypeError: The configuration has an invalid type.
-        """
-        match config:
-            case str():
-                return cls.model_validate({"name": config})
-            case dict():
-                return cls.model_validate(config)
-            case _:
-                raise TypeError(f"Invalid configuration type {type(config)}")
--- src/anycastd/_configuration/prefix/_frr.py
+++ src/anycastd/_configuration/prefix/_frr.py
@@ -1,6 +1,5 @@
 from ipaddress import IPv4Network, IPv6Network
 from pathlib import Path
-from typing import Self
 
 from anycastd._configuration.sub import PrefixConfiguration
 
@@ -17,29 +16,3 @@ class FRRPrefix(PrefixConfiguration):
     prefix: IPv4Network | IPv6Network
     vrf: int | None = None
     vtysh: Path = Path("/usr/bin/vtysh")
-
-    @classmethod
-    def from_configuration(cls, config: str | dict) -> Self:
-        """Create an instance from the configuration format.
-
-        The configuration can be provided in two formats, a simplified format
-        containing only the prefix as a string, or a full format containing the
-        prefix as well as other optional options.
-
-        Args:
-            config: The configuration for the prefix.
-
-        Returns:
-            A new FRRPrefix instance.
-
-        Raises:
-            ValidationError: Failed to validate the configuration.
-            TypeError: The configuration has an invalid type.
-        """
-        match config:
-            case str():
-                return cls.model_validate({"prefix": config})
-            case dict():
-                return cls.model_validate(config)
-            case _:
-                raise TypeError(f"Invalid configuration type {type(config)}")
