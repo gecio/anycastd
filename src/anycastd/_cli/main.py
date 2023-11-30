36995d2e071eef07a43d12976fbaf6857e4fe8a2
Marvin Vogt <m@rvinvogt.com>
2023-11-30 11:18:21 +0000

    Add func to get main config and handle errors

--- src/anycastd/_cli/main.py
+++ src/anycastd/_cli/main.py
@@ -2,6 +2,10 @@ from pathlib import Path
 from typing import Annotated
 
 import typer
+from pydantic import ValidationError
+
+from anycastd._cli.output import ExitCode, print_error
+from anycastd._configuration import ConfigurationError, MainConfiguration
 
 CONFIG_PATH = Path("/etc/anycastd/config.toml")
 
@@ -9,7 +13,7 @@ app = typer.Typer()
 
 
 @app.callback()
-def callback():
+def callback() -> None:
     """Manage anycasted services based on status checks."""
 
 
@@ -22,7 +26,30 @@ def run(
             "-c",
             help="Location of the configuration file.",
             envvar="CONFIG",
+            readable=False,
+            resolve_path=True,
         ),
     ] = CONFIG_PATH,
 ) -> None:
     """Run anycastd."""
+
+
+def _get_main_configuration(config: Path) -> MainConfiguration:
+    """Get the main configuration object from a path to a TOML file.
+
+    Try to read the configuration file while exiting with an appropriate exit
+    code if an error occurs.
+    """
+    try:
+        return MainConfiguration.from_toml_file(config)
+    except ConfigurationError as exc:
+        match exc.__cause__:
+            case FileNotFoundError() | PermissionError():
+                exit_code = ExitCode.NOINPUT
+            case IsADirectoryError():
+                exit_code = ExitCode.IOERR
+            case KeyError() | ValueError() | TypeError() | ValidationError():
+                exit_code = ExitCode.DATAERR
+            case _:
+                exit_code = ExitCode.ERR
+        print_error(exc, exit_code=exit_code)
