e01209b3300a429cf54c088bf36479fc4d727dea
Marvin Vogt <m@rvinvogt.com>
2023-12-04 10:07:56 +0000

    Add tests for conversion of config objects to instances

---
+++ tests/configuration/test_conversion.py
@@ -0,0 +1,53 @@
+import datetime
+from ipaddress import IPv6Network
+from pathlib import Path
+
+import pytest
+from anycastd._configuration.conversion import config_to_instance
+from anycastd._configuration.healthcheck import (
+    CabourotteHealthcheckConfiguration,
+    HealthcheckConfiguration,
+)
+from anycastd._configuration.prefix import FRRPrefixConfiguration, PrefixConfiguration
+from anycastd._executor import LocalExecutor
+from anycastd.healthcheck import CabourotteHealthcheck, Healthcheck
+from anycastd.prefix import FRRoutingPrefix, Prefix
+
+
+@pytest.mark.parametrize(
+    "config,expected",
+    [
+        (
+            CabourotteHealthcheckConfiguration(
+                name="test cabourotte healthcheck",
+                url="http://[::]:9013",
+                interval=datetime.timedelta(minutes=1),
+            ),
+            CabourotteHealthcheck(
+                name="test cabourotte healthcheck",
+                url="http://[::]:9013",
+                interval=datetime.timedelta(minutes=1),
+            ),
+        ),
+        (
+            FRRPrefixConfiguration(
+                prefix=IPv6Network("2001:db8::/32"),
+                vrf="77",
+                vtysh=Path("/run/vtysh"),
+            ),
+            FRRoutingPrefix(
+                prefix=IPv6Network("2001:db8::/32"),
+                vrf="77",
+                vtysh=Path("/run/vtysh"),
+                executor=LocalExecutor(),
+            ),
+        ),
+    ],
+)
+def test_subconfiguration_converted_to_instance(
+    config: PrefixConfiguration | HealthcheckConfiguration,
+    expected: Prefix | Healthcheck,
+):
+    """A subconfiguration can be converted to an instance of the type it represents."""
+    converted = config_to_instance(config)
+    assert converted == expected
