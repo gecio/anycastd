a193e9aef5696c72a2b9a2a8866321ef812cb8d3
Marvin Vogt <m@rvinvogt.com>
2023-12-05 15:27:19 +0000

    Implement conversion

--- src/anycastd/_configuration/conversion.py
+++ src/anycastd/_configuration/conversion.py
@@ -1,9 +1,13 @@
 from typing import overload
 
-from anycastd._configuration.healthcheck import HealthcheckConfiguration
-from anycastd._configuration.prefix import PrefixConfiguration
-from anycastd.healthcheck import Healthcheck
-from anycastd.prefix import Prefix
+from anycastd._configuration.healthcheck import (
+    CabourotteHealthcheckConfiguration,
+    HealthcheckConfiguration,
+)
+from anycastd._configuration.prefix import FRRPrefixConfiguration, PrefixConfiguration
+from anycastd._executor import LocalExecutor
+from anycastd.healthcheck import CabourotteHealthcheck, Healthcheck
+from anycastd.prefix import FRRoutingPrefix, Prefix
 
 
 @overload
@@ -20,3 +24,12 @@ def config_to_instance(
     config: PrefixConfiguration | HealthcheckConfiguration
 ) -> Prefix | Healthcheck:
     """Convert a configuration to an instance of it's respective type."""
+    match config:
+        case FRRPrefixConfiguration():
+            return FRRoutingPrefix(**config.model_dump(), executor=LocalExecutor())
+        case CabourotteHealthcheckConfiguration():
+            return CabourotteHealthcheck(**config.model_dump())
+        case _:
+            raise NotImplementedError(
+                f"Configuration type {type(config)} is not supported."
+            )
