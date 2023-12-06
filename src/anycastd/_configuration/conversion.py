4a727ca4b7882950d1ac10cb1d666708e99d65fb
Marvin Vogt <m@rvinvogt.com>
2023-12-06 14:57:16 +0100

    Run anycastd from `MainConfiguration` using the run command (#26)
    * Add function to convert `ServiceConfiguration` to `Service` instance
    
    * Raname `config_to_instance` and improve docstring
    
    * Expose `config_to_service` from configuration module instead of `_sub_config_to_instance`
    
    * Expose `run_from_configuration` from core module
    
    * Call `run_from_configuration` in CLI module
    
    * Add tests for run CLI command
    
    * Fix typo "pararmeters"

--- src/anycastd/_configuration/conversion.py
+++ src/anycastd/_configuration/conversion.py
@@ -5,25 +5,56 @@ from anycastd._configuration.healthcheck import (
     HealthcheckConfiguration,
 )
 from anycastd._configuration.prefix import FRRPrefixConfiguration, PrefixConfiguration
+from anycastd._configuration.service import ServiceConfiguration
 from anycastd._executor import LocalExecutor
+from anycastd.core._service import Service
 from anycastd.healthcheck import CabourotteHealthcheck, Healthcheck
 from anycastd.prefix import FRRoutingPrefix, Prefix
 
 
+def config_to_service(config: ServiceConfiguration) -> Service:
+    """Convert a service configuration to an actual service instance.
+
+    Args:
+        config: The configuration to convert.
+
+    Returns:
+        A service instance with the parameters from the configuration.
+    """
+    prefixes: tuple[Prefix, ...] = tuple(
+        _sub_config_to_instance(prefix) for prefix in config.prefixes
+    )
+    health_checks: tuple[Healthcheck, ...] = tuple(
+        _sub_config_to_instance(check) for check in config.checks
+    )
+
+    return Service(name=config.name, prefixes=prefixes, health_checks=health_checks)
+
+
 @overload
-def config_to_instance(config: PrefixConfiguration) -> Prefix:
+def _sub_config_to_instance(config: PrefixConfiguration) -> Prefix:
     ...
 
 
 @overload
-def config_to_instance(config: HealthcheckConfiguration) -> Healthcheck:
+def _sub_config_to_instance(config: HealthcheckConfiguration) -> Healthcheck:
     ...
 
 
-def config_to_instance(
+def _sub_config_to_instance(
     config: PrefixConfiguration | HealthcheckConfiguration,
 ) -> Prefix | Healthcheck:
-    """Convert a configuration to an instance of it's respective type."""
+    """Convert a subconfiguration to an instance of it's respective type.
+
+    Convert subconfigurations to instances of the repsctive type
+    they are describing, e.g. a FRRPrefixConfiguration to a FRRoutingPrefix.
+
+    Args:
+        config: The subconfiguration to convert.
+
+    Returns:
+        An instance of the respective type the subconfiguration describes.
+    """
     match config:
         case FRRPrefixConfiguration():
             return FRRoutingPrefix(**config.model_dump(), executor=LocalExecutor())
--- tests/cli/test_main.py
+++ tests/cli/test_main.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+from unittest.mock import MagicMock
+
 import anycastd
+import pytest
 
 
 def test_version_displayed_correctly(anycastd_cli):
@@ -9,3 +13,52 @@ def test_version_displayed_correctly(anycastd_cli):
 
     assert result.exit_code == 0
     assert result.stdout == expected
+
+
+class TestRunCMD:
+    """Test the run command."""
+
+    @pytest.fixture
+    def mock_configuration(self, mocker):
+        """An autospecced mock instance of the MainConfiguration class."""
+        return mocker.create_autospec(
+            "anycastd._configuration.MainConfiguration", spec_set=True, instance=True
+        )
+
+    @pytest.fixture
+    def mock_run_from_configuration(self, mocker) -> MagicMock:
+        """An autospecced mock of the run_from_configuration function."""
+        return mocker.patch("anycastd._cli.main.run_from_configuration", autospec=True)
+
+    @pytest.fixture
+    def mock_get_main_configuration(self, mocker, mock_configuration) -> MagicMock:
+        """An autospecced mock of the _get_main_configuration function."""
+        mock = mocker.patch("anycastd._cli.main._get_main_configuration", autospec=True)
+        mock.return_value = mock_configuration
+        return mock
+
+    def test_calls_get_main_configuration_w_config_path(
+        self, anycastd_cli, mock_get_main_configuration, mock_run_from_configuration
+    ):
+        """_get_main_configuration is called with the config path."""
+        config = Path("/path/to/config.toml")
+
+        anycastd_cli("run", "--config", config.as_posix())
+
+        mock_get_main_configuration.assert_called_once_with(config)
+
+    def test_calls_run_from_configuration(
+        self,
+        anycastd_cli,
+        mock_configuration,
+        mock_run_from_configuration,
+        mock_get_main_configuration,
+    ):
+        """run_from_configuration is called.
+
+        Run from configuration is called with the main configuration object
+        obtained from the configuration file path.
+        """
+        anycastd_cli("run")
+
+        mock_run_from_configuration.assert_called_once_with(mock_configuration)
