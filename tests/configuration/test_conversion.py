ccda4495650fbeeeb3e7cf35eab09a757894eb3f
Marvin Vogt <m@rvinvogt.com>
2023-12-08 14:06:51 +0000

    Add `dict_w_items_named_by_key_to_flat_w_name_value` function

--- tests/configuration/test_conversion.py
+++ tests/configuration/test_conversion.py
@@ -3,7 +3,10 @@ from ipaddress import IPv6Network
 from pathlib import Path
 
 import pytest
-from anycastd._configuration.conversion import _sub_config_to_instance
+from anycastd._configuration.conversion import (
+    _sub_config_to_instance,
+    dict_w_items_named_by_key_to_flat_w_name_value,
+)
 from anycastd._configuration.healthcheck import (
     CabourotteHealthcheckConfiguration,
     HealthcheckConfiguration,
@@ -50,4 +53,14 @@ def test_subconfiguration_converted_to_instance(
 ):
     """A subconfiguration can be converted to an instance of the type it represents."""
     converted = _sub_config_to_instance(config)
+    assert converted == expected
+
+
+def test_dict_w_named_to_flat_w_name_value():
+    """Dictionaries with named items can be converted to flat dictionaries."""
+    named = {"foo": {"bar": "baz"}, "qux": {"quux": "corge"}}
+    expected = ({"name": "foo", "bar": "baz"}, {"name": "qux", "quux": "corge"})
+
+    converted = dict_w_items_named_by_key_to_flat_w_name_value(named)
+
     assert converted == expected
