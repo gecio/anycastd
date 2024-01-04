a56d642799a7b2df170b6a9634cb81f84840dd12
Marvin Vogt <m@rvinvogt.com>
2024-01-04 11:56:40 +0100

    Update dev dependencies (#35)
    * Update dev dependencies
    
    * Remove `text` parameter to `create_subprocess_exec` on `Executor`
    Although undocumented, `asyncio.create_subprocess_exec` does not fully support all `subprocess.Popen` parameters

--- src/anycastd/_executor.py
+++ src/anycastd/_executor.py
@@ -11,14 +11,13 @@ class Executor(ABC):
     """An interface to execute programs."""
 
     @abstractmethod
-    async def create_subprocess_exec(  # noqa: PLR0913
+    async def create_subprocess_exec(
         self,
         exec: str | Path,
         args: Sequence[str],
         *,
         stdout: int | IO[Any] | None = subprocess.PIPE,
         stderr: int | IO[Any] | None = subprocess.PIPE,
-        text: bool = False,
     ) -> asyncio.subprocess.Process:
         """Create an async subprocess.
 
@@ -27,7 +26,6 @@ class Executor(ABC):
             args: The arguments to pass to the program.
             stdout: The stdout file handle as specified in subprocess.Popen.
             stderr: The stderr file handle as specified in subprocess.Popen.
-            text: Whether to decode the output as text.
 
         Returns:
             An asyncio.subprocess.Process object.
@@ -39,14 +37,13 @@ class Executor(ABC):
 class LocalExecutor(Executor):
     """An executor that runs commands locally."""
 
-    async def create_subprocess_exec(  # noqa: PLR0913
+    async def create_subprocess_exec(
         self,
         exec: str | Path,
         args: Sequence[str],
         *,
         stdout: int | IO[Any] | None = subprocess.PIPE,
         stderr: int | IO[Any] | None = subprocess.PIPE,
-        text: bool = False,
     ) -> asyncio.subprocess.Process:
         """Create an async subprocess.
 
@@ -57,13 +54,12 @@ class LocalExecutor(Executor):
             args: The arguments to pass to the program.
             stdout: The stdout file handle as specified in subprocess.Popen.
             stderr: The stderr file handle as specified in subprocess.Popen.
-            text: Whether to decode the output as text.
 
         Returns:
             An asyncio.subprocess.Process object.
         """
         return await asyncio.create_subprocess_exec(
-            exec, *args, stdout=stdout, stderr=stderr, text=text
+            exec, *args, stdout=stdout, stderr=stderr
         )
 
 
@@ -79,14 +75,13 @@ class DockerExecutor(Executor):
     docker: Path
     container: str
 
-    async def create_subprocess_exec(  # noqa: PLR0913
+    async def create_subprocess_exec(
         self,
         exec: str | Path,
         args: Sequence[str],
         *,
         stdout: int | IO[Any] | None = subprocess.PIPE,
         stderr: int | IO[Any] | None = subprocess.PIPE,
-        text: bool = False,
     ) -> asyncio.subprocess.Process:
         """Create an async subprocess inside of a Docker container.
 
@@ -98,12 +93,11 @@ class DockerExecutor(Executor):
             args: The arguments to pass to the program.
             stdout: The stdout file handle as specified in subprocess.Popen.
             stderr: The stderr file handle as specified in subprocess.Popen.
-            text: Whether to decode the output as text.
 
         Returns:
             An asyncio.subprocess.Process object.
         """
         docker_args = ("exec", "-i", self.container, exec, *args)
         return await asyncio.create_subprocess_exec(
-            self.docker, *docker_args, stdout=stdout, stderr=stderr, text=text
+            self.docker, *docker_args, stdout=stdout, stderr=stderr
         )
