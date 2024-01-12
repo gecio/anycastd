b73e4fafc7f6cbc48ef79b83a2e0402389badd0c
Marvin Vogt <m@rvinvogt.com>
2024-01-12 18:00:52 +0000

    Add test for missing top level configuration key

--- tests/configuration/test_main_configuration.py
+++ tests/configuration/test_main_configuration.py
@@ -1,3 +1,5 @@
+import pytest
+from anycastd._configuration.exceptions import ConfigurationSyntaxError
 from anycastd._configuration.main import MainConfiguration
 
 
@@ -6,3 +8,12 @@ def test_initialized_from_valid_toml(sample_configuration, sample_configuration_
     path, _ = sample_configuration_file
     config = MainConfiguration.from_toml_file(path)
     assert config == sample_configuration
+
+
+def test_missing_required_top_level_field_raises(sample_configuration_dict):
+    """Exception raised when a required top-level field is missing."""
+    expected = r".*missing required key 'services'.*"
+    del sample_configuration_dict["services"]
+
+    with pytest.raises(ConfigurationSyntaxError, match=expected):
+        MainConfiguration.from_configuration_dict(sample_configuration_dict)
