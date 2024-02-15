64115d323d91f4c61238e4a2e78362fb55aca141
Marvin Vogt <m@rvinvogt.com>
2024-02-15 16:14:53 +0000

    Remove stdout and stderr args while renaming `exec` to `program` to match stdlib and avoid using a keyword

--- src/anycastd/_executor.py
+++ src/anycastd/_executor.py
@@ -1,10 +1,8 @@
 import asyncio
-import subprocess
 from abc import ABC, abstractmethod
 from collections.abc import Sequence
 from dataclasses import dataclass
 from pathlib import Path
-from typing import IO, Any
 
 
 class Executor(ABC):
@@ -12,20 +10,13 @@ class Executor(ABC):
 
     @abstractmethod
     async def create_subprocess_exec(
-        self,
-        exec: str | Path,
-        args: Sequence[str],
-        *,
-        stdout: int | IO[Any] | None = subprocess.PIPE,
-        stderr: int | IO[Any] | None = subprocess.PIPE,
+        self, program: str | Path, args: Sequence[str]
     ) -> asyncio.subprocess.Process:
         """Create an async subprocess.
 
         Args:
-            exec: The path of the program to execute.
+            program: The path of the program to execute.
             args: The arguments to pass to the program.
-            stdout: The stdout file handle as specified in subprocess.Popen.
-            stderr: The stderr file handle as specified in subprocess.Popen.
 
         Returns:
             An asyncio.subprocess.Process object.
@@ -38,29 +29,20 @@ class LocalExecutor(Executor):
     """An executor that runs commands locally."""
 
     async def create_subprocess_exec(
-        self,
-        exec: str | Path,
-        args: Sequence[str],
-        *,
-        stdout: int | IO[Any] | None = subprocess.PIPE,
-        stderr: int | IO[Any] | None = subprocess.PIPE,
+        self, program: str | Path, args: Sequence[str]
     ) -> asyncio.subprocess.Process:
         """Create an async subprocess.
 
         This method simply wraps asyncio.create_subprocess_exec.
 
         Args:
-            exec: The path of the program to execute.
+            program: The path of the program to execute.
             args: The arguments to pass to the program.
-            stdout: The stdout file handle as specified in subprocess.Popen.
-            stderr: The stderr file handle as specified in subprocess.Popen.
 
         Returns:
             An asyncio.subprocess.Process object.
         """
-        return await asyncio.create_subprocess_exec(
-            exec, *args, stdout=stdout, stderr=stderr
-        )
+        return await asyncio.create_subprocess_exec(program, *args)
 
 
 @dataclass
@@ -76,12 +58,7 @@ class DockerExecutor(Executor):
     container: str
 
     async def create_subprocess_exec(
-        self,
-        exec: str | Path,
-        args: Sequence[str],
-        *,
-        stdout: int | IO[Any] | None = subprocess.PIPE,
-        stderr: int | IO[Any] | None = subprocess.PIPE,
+        self, program: str | Path, args: Sequence[str]
     ) -> asyncio.subprocess.Process:
         """Create an async subprocess inside of a Docker container.
 
@@ -89,15 +66,11 @@ class DockerExecutor(Executor):
         the command inside of a Docker container.
 
         Args:
-            exec: The path of the program to execute.
+            program: The path of the program to execute.
             args: The arguments to pass to the program.
-            stdout: The stdout file handle as specified in subprocess.Popen.
-            stderr: The stderr file handle as specified in subprocess.Popen.
 
         Returns:
             An asyncio.subprocess.Process object.
         """
-        docker_args = ("exec", "-i", self.container, exec, *args)
-        return await asyncio.create_subprocess_exec(
-            self.docker, *docker_args, stdout=stdout, stderr=stderr
-        )
+        docker_args = ("exec", "-i", self.container, program, *args)
+        return await asyncio.create_subprocess_exec(self.docker, *docker_args)
