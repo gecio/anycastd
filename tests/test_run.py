992f1db95dafa5199f1c509f2484e44f9894e8b0
Marvin Vogt <m@rvinvogt.com>
2024-02-15 13:34:22 +0000

    Add test for service future creation

---
+++ tests/test_run.py
@@ -0,0 +1,16 @@
+import pytest
+from anycastd.core._run import run_services
+from anycastd.core._service import Service
+
+
+@pytest.fixture
+def mock_services(mocker):
+    """A set of mock services."""
+    return [mocker.AsyncMock(Service) for _ in range(3)]
+
+
+@pytest.mark.asyncio
+async def test_future_created_for_each_service(mock_services):
+    """A future is created for each service."""
+    await run_services(mock_services)
+    assert all(mock_service.run.called for mock_service in mock_services)
