c92dff2cdd85791e5b0d35dda76e93e0691a4289
Marvin Vogt <m@rvinvogt.com>
2024-02-19 11:28:18 +0100

    Add tests for and fix entrypoint version check (#46)
    * Add tests for entrypoint version check
    
    * Fix minimum version check

--- src/anycastd/__main__.py
+++ src/anycastd/__main__.py
@@ -6,7 +6,7 @@ MIN_PYTHON_VERSION = (3, 11)
 
 def _check_python_version() -> None:
     """Check that minimum Python version requirements are met."""
-    if sys.version_info <= MIN_PYTHON_VERSION:
+    if sys.version_info < MIN_PYTHON_VERSION:
         sys.exit(
             f"Python >= {MIN_PYTHON_VERSION[0]}.{MIN_PYTHON_VERSION[1]} "
             "is required to run anycastd."
