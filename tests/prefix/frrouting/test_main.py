bcf63f4b2e96a18d2437ab445ea022fe3e697321
Marvin Vogt <m@rvinvogt.com>
2024-02-26 15:41:00 +0000

    Add test for not announced warning

--- tests/prefix/frrouting/test_main.py
+++ tests/prefix/frrouting/test_main.py
@@ -2,7 +2,9 @@ from ipaddress import IPv6Network
 from pathlib import Path
 
 from anycastd._executor import LocalExecutor
+from anycastd.prefix._frrouting.exceptions import FRRCommandError
 from anycastd.prefix._frrouting.main import FRRoutingPrefix
+from structlog.testing import capture_logs
 
 
 def test_repr(example_networks, example_vrfs):
@@ -41,3 +43,25 @@ def test_non_equal():
         prefix=IPv6Network("2001:db8::/32"), vrf="43", executor=LocalExecutor()
     )
     assert prefix1 != prefix2
+
+
+async def test_denouncing_non_announced_logs_warning(mocker):
+    """Denouncing a non-announced prefix logs a warning."""
+    prefix = FRRoutingPrefix(
+        prefix=IPv6Network("2001:db8::/32"), vrf="42", executor=LocalExecutor()
+    )
+    exc = FRRCommandError(
+        ["test command"], 1, stdout=None, stderr="Can't find static route specified"
+    )
+    mocker.patch.object(prefix, "_get_local_asn")
+    mocker.patch.object(prefix, "_run_vtysh_commands", side_effect=exc)
+
+    with capture_logs() as logs:
+        await prefix.denounce()
+
+    assert logs[0]["event"] == "Attempted to denounce prefix that was not announced."
+    assert logs[0]["log_level"] == "warning"
+    assert logs[0]["prefix"] == str(prefix.prefix)
+    assert logs[0]["prefix_type"] == "FRRoutingPrefix"
+    assert logs[0]["prefix_vrf"] == prefix.vrf
+    assert logs[0]["vtysh_path"] == str(prefix.vtysh)
