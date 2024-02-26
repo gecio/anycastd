6321519ccb7ca8cb2482ee78bc63f6a5dd135b9c
Marvin Vogt <m@rvinvogt.com>
2024-02-26 14:24:43 +0000

    Remove reduntant log line

--- src/anycastd/core/_run.py
+++ src/anycastd/core/_run.py
@@ -1,18 +1,13 @@
 import asyncio
 from collections.abc import Iterable
 
-import structlog
-
 from anycastd._configuration import MainConfiguration, config_to_service
 from anycastd.core._service import Service
-
-logger = structlog.get_logger()
 
 
 async def run_from_configuration(configuration: MainConfiguration) -> None:
     """Run anycastd using an instance of the main configuration."""
     services = tuple(config_to_service(config) for config in configuration.services)
-    logger.debug("Running services.", services=services)
     await run_services(services)
 
 
