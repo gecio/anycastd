fe7b1b6b7cf0e6bd1dba1d20bb55dbc1139220c1
Marvin Vogt <m@rvinvogt.com>
2024-02-28 10:36:44 +0000

    Add fixture to reset structlog configuration after using the CLI fixture

--- tests/cli/conftest.py
+++ tests/cli/conftest.py
@@ -1,14 +1,30 @@
-from collections.abc import Callable
+from collections.abc import Callable, Iterator
 
 import pytest
+import structlog
 from anycastd._cli.main import app
 from click.testing import Result
 from typer.testing import CliRunner
 
 
 @pytest.fixture
-def anycastd_cli() -> Callable[..., Result]:
+def reset_structlog_config() -> Iterator[None]:
+    """Reset structlog configuration after running a test.
+
+    This is required for tests that invoke the anycastd CLI, as running it
+    initializes the structlog configuration, adding filters that could affect
+    other tests.
+    """
+    yield
+    structlog.reset_defaults()
+
+
+@pytest.fixture
+def anycastd_cli(reset_structlog_config) -> Callable[..., Result]:
     """A callable that runs anycastd CLI commands.
+
+    To make sure that this fixture does not affect other tests, state that is
+    initialized when running the CLI needs to be reset after using this fixture.
 
     Example:
     ```python
